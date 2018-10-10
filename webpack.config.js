04620dddff78718526622d6e9d66221b15b6bb32
warren96 <gokikuyama@yahoo.co.jp>
2018-10-10 21:17:20 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const htmlWebpackplugin = require('html-webpack-plugin'); 
+
+module.exports={
+    mode: 'production',
+    entry:'./src/index.tsx',
+    module:{
+        rules:[
+            {
+                enforce:'pre',
+                loader:'tslint-loader',
+                test:/\.tsx?$/,
+                exclude:[
+                    /node_modules/
+                ],
+                options:{
+                    emitErrors:true
+                }
+            },
+            {
+                loader:'ts-loader',
+                test:/\.tsx?$/,
+                exclude:[
+                    /node_modules/
+                ],
+                options:{
+                    configFile:'tsconfig.prod.json'
+                }
+            }
+        ]
+    },
+    resolve:{
+        extensions:['.tsx','ts','js']
+    },
+    output:{
+        filename:'static/js/bundle.js',
+        path:path.resolve(_dirname,'dist')
+},
+plugins:[
+    new htmlWebpackPlugin({
+        template:"index.html"
+
+    })
+]
+};
